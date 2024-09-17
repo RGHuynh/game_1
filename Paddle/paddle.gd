d24d4c5d3b2c8466e148c452c0f33bf92a5f5d88
Mrbrokenlegs <randall@lunchbox.io>
2024-09-17 10:05:29 -0400

    Add input to paddle

---
+++ Paddle/paddle.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+const SPEED:int = 500
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("ui_down"):
+		position.y += 1 * SPEED * delta
+	if Input.is_action_pressed("ui_up"):
+		position.y -= 1 * SPEED * delta
+	pass
