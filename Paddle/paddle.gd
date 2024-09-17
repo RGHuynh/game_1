04df696936b8c1132a6e6f0f9177644090644e07
Mrbrokenlegs <randall@lunchbox.io>
2024-09-17 10:42:52 -0400

    Add clamp to paddle

--- Paddle/paddle.gd
+++ Paddle/paddle.gd
@@ -1,10 +1,13 @@
 extends StaticBody2D
 
 const SPEED:int = 500
+var window_height
+var player_heigth
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	window_height = get_viewport_rect().size
+	player_heigth = $ColorRect.size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,4 +16,11 @@ func _process(delta):
 		position.y += 1 * SPEED * delta
 	if Input.is_action_pressed("ui_up"):
 		position.y -= 1 * SPEED * delta
-	pass
+	
+	print(window_height.y - player_heigth.y/2)
+	position.y = clamp(position.y, Vector2.ZERO.y, window_height.y - player_heigth.y)
+
+
+
+	
+	
